7253fd92b4d7139c80a9e415ececf72d12cb6e78
Jorge Plaza de los Reyes <japlazadelosreyes@miuandes.cl>
2021-10-01 16:13:59 -0300

    1st commit

---
+++ assemblerv1.py
@@ -0,0 +1,17 @@
+def recibirArchivo(data):
+    print()
+    return data
+
+
+def parser():
+    pass
+
+def error():
+    pass
+
+def archivoOut():
+    pass
+
+def opTable():
+    opcodes = {}
+    return
