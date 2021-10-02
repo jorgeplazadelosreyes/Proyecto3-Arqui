735215fb2e9e0a8781d58466bfa3587017c2ba40
Jorge Plaza de los Reyes <japlazadelosreyes@miuandes.cl>
2021-10-02 18:26:12 -0300

    3rd commit

--- assemblerv1.py
+++ assemblerv1.py
@@ -1,19 +1,28 @@
-opcodes = ["MOV", "SUB", "AND", "OR", "NOT", "XOR", "SHL", "SHR", "INC", "RST", "CMP", "JMP", "JEQ", "JNE", "JGT", "JLT", "JGE", "JLE", "JCR", "JOV"]
+opcodes = ["MOV", "SUB", "AND", "OR", "NOT", "XOR", "SHL", "SHR", "INC", "RST", "CMP", "JMP", "JEQ", "JNE", "JGT", "JLT", "JGE", "JLE", "JCR", "JOV", "CALL", "RET", "POP", "PUSH"]
 functions = []
 
-def leerArchivo(data):
+def leerCodigo(data):
     archivo = open(data, 'r')
     line = archivo.readline()
+    counter = 1             ## Contador de lineas
+    flag = False            ## flag si hay errores en el archivo 
     while(line):
         parsed = line.split(" ")
-        if '' in parsed:
-            parsed.remove('')
         print(parsed)
+        if len(parsed) == 1:
+            call = parsed[0].replace('\n','').replace(':','')
+            if call not in functions:
+                functions.append(call)
+            else:
+                print(f"Error: Redefinicion de funcion {call}, linea: {counter} ")
+                flag = True
+
         line = archivo.readline()
+        counter += 1
     archivo.close()
 
 
-def parser():
+def leerData():
     pass
 
 def error():
@@ -25,6 +34,6 @@ def archivoOut():
 
 def main():
     data =  "p3F_1.ass" ##input("Ingrese archivo .ass: ")
-    leerArchivo(data)
+    leerCodigo(data)
 
 main()
